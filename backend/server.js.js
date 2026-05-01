82eac940b2ef9355654a465f90f57d93b0927512
markkingoo159-star <markkingoo159@gmail.com>
2026-05-01 13:16:42 +0300

    Add files via upload

---
+++ backend/server.js.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let users = [];
+
+// Register
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  users.push({ username, password, balance: 1000 });
+  res.json({ message: "User registered" });
+});
+
+// Login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
+    res.json({ message: "Login success", user });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+// Balance
+app.get("/balance/:username", (req, res) => {
+  const user = users.find((u) => u.username === req.params.username);
+  res.json({ balance: user ? user.balance : 0 });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
